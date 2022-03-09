8913ce1d4a6a6f38dbf29bf3a802992ed7dd05ef
Gowtham <sgowtham543@gmail.com>
2022-03-09 20:15:45 +0530

    Project Creation and Test Commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by S, Gowtham (G.) on 09/03/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
